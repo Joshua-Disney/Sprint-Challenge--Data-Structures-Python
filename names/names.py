c24ae15a7f93073b324495664190e9ec462e1dc0
joshua-disney <joshua.a.disney@gmail.com>
2019-09-20 11:28:45 -0700

    Names runtime down to 1.123018503189087 seconds

--- names/names.py
+++ names/names.py
@@ -9,12 +9,22 @@ f.close()
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
+# <----------------original
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# duplicates = []
+# for name in names_1:
+#     if name in names_2:
+#         duplicates.append(name)
+
+# duplicates = names_1 - [i for i in names_2 if i not in names_1]   <------ didn't work
+
+duplicates = [i for i in names_2 if i in names_1]
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
