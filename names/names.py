c87329e390e02296dd4d26ef6b0107642a1c24af
joshua-disney <joshua.a.disney@gmail.com>
2019-09-20 09:28:29 -0700

    Majority of ring_buffer.  Still displaying None

--- names/names.py
+++ names/names.py
@@ -19,5 +19,3 @@ for name_1 in names_1:
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
-
-# Init
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,42 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        else:
+            self.storage[self.current] = item
+        if self.current < self.capacity - 1:
+            self.current += 1
+        else:
+            self.current = 0
 
-  def get(self):
-    pass
+    def get(self):
+        print(f"current: {self.current}")
+        print(f"item at: {self.storage[self.current - 1]}")
+        print(f"full list: {self.storage}")
+        pass
+
+
+rb = RingBuffer(5)
+rb.append("first")
+rb.get()
+rb.append("second")
+rb.get()
+rb.append("third")
+rb.get()
+rb.append("fourth")
+rb.get()
+rb.append("fifth")
+rb.get()
+rb.append("sixth")
+rb.get()
+rb.append("seventh")
+rb.get()
+rb.append("eighth")
+rb.get()
+rb.append("nineth")
+rb.get()
